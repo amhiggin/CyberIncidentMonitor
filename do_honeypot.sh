db3b87ce0aef527abef026d1685664b5502cfba0
Amber <amhiggin@tcd.ie>
2018-02-08 15:56:36 +0000

    Update do_honeypot.sh
    Adding definition of router as part of network setup, moving the setup of the container out of the 'build' function (which should just be used to build the base image)

--- do_honeypot.sh
+++ do_honeypot.sh
@@ -25,7 +25,12 @@ esac
 			
 # Build the cowrie image
 build() {
-	docker build -t cowrie -f ./DockerFile .
+        docker build -t cowrie -f ./DockerFile .
+        echo "Built cowrie image successfully."
+}
+
+# Run the docker container in the background, giving it name "CONTAINER_NAME"
+run() {
         # Create the required host directories if they don't already exist
         mkdir -p cowrievolumes/$CONTAINER_NAME/dl       # Capture malware checksums
         mkdir -p cowrievolumes/$CONTAINER_NAME/log      # Capture logs
@@ -37,15 +42,16 @@ build() {
         cp "$(pwd)"/userdb.txt cowrievolumes/$CONTAINER_NAME/data/
         cp "$(pwd)"/cowrie.cfg.dist cowrievolumes/$CONTAINER_NAME/cowrie.cfg
 
-}
+        # define dmz net
+        create_dmz_net
 
-# Run the docker container in the background, giving it name "CONTAINER_NAME"
-run() {
-	create_dmz_net()
-	docker run -d --network="dmz" --name $CONTAINER_NAME \
-		-p 22:22 -p 23:23 \
-		-v ~/cowrievolumes/$CONTAINER_NAME/etc:/cowrie-cowrie-git/etc \
-		-v ~/cowrievolumes/$CONTAINER_NAME/var:/cowrie-cowrie-git/var cowrie:latest
+        # run the container on network dmz mounting the volumes
+        docker run -d --network="dmz" --name $CONTAINER_NAME \
+                -p 2222:2222 -p 2223:2223 \
+                -v ~/cowrievolumes/$CONTAINER_NAME/dl:/cowrie-cowrie-git/dl \
+                -v ~/cowrievolumes/$CONTAINER_NAME/log:/cowrie-cowrie-git/log \
+                -v ~/cowrievolumes/$CONTAINER_NAME/data:/cowrie-cowrie-git/data cowrie:latest
+        echo "Container $CONTAINER_NAME running"
 }
 
 # Start the docker container
@@ -66,6 +72,7 @@ remove() {
 
 }
 
+# Local DMZ bridge network to which all containers are connected
 create_dmz_net() {
         network_exists=$( sudo docker network ls | grep "dmz" ) 
         if [[ -n "$network_exists" ]] ; then
@@ -73,6 +80,20 @@ create_dmz_net() {
         else
                 echo "Creating DMZ network (10.0.0.0/8)"
                 docker network create -d bridge --subnet 10.0.0.0/8 dmz
+        fi
+
+        # define a router
+        define_router
+}
+
+define_router() {
+        router_defined=$(sudo docker ps -a | grep "router" )
+        if [[ -n "$router_defined" ]] ; then
+                echo "Router defined"
+        else
+                docker create --name router --cap-add=NET_ADMIN -ti cowrie /bin/bash
+                docker network connect dmz router --ip "10.0.0.254"
+                echo "New router defined for network DMZ"
         fi
 }
 
