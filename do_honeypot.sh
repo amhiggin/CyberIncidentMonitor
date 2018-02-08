2bfec145e1d9c73a7052226c39775609b10e08a6
Amber <amhiggin@tcd.ie>
2018-02-08 11:25:04 +0000

    Update do_honeypot.sh

--- do_honeypot.sh
+++ do_honeypot.sh
@@ -1,19 +1,42 @@
-'''
-	Docker script for carrying out operations on a specified docker container.
-	Takes the container name as $1, used in the creation of directories etc.
-'''
-CONTAINER_NAME = "$1"
+#!/bin/bash
 
+CONTAINER_NAME="$2"
+INSTRUCTION="$1"
+
+main() {
+        case $INSTRUCTION in
+                build)
+                        build ;;
+                run)    
+                        run ;;
+                start)
+                        start ;;
+                stop)
+                        stop ;;
+                remove)
+                        remove ;;
+                create_dmz_net)
+                        create_dmz_net ;;
+		*)
+                        echo $"Usage: $0 {build|run|start|stop|remove|create_dm$
+                        exit 1  
+esac
+}		
+			
 # Build the cowrie image
 build() {
-	# TODO have to add the Dockerfile to my Github
-	docker build -t amhiggin/cowrie http://www.github.com/amhiggin/CyberIncidentMonitor/master/Dockerfile .
-	# Create the required host directories if they don't already exist
-	mkdir -p cowrievolumes/$CONTAINER_NAME/dl	# Capture malware checksums
-	mkdir -p cowrievolumes/$CONTAINER_NAME/log	# Capture logs
-	mkdir -p cowrievolumes/$CONTAINER_NAME/data	# Add custom userdb.txt 
-	cp "$(pwd)"/cowrie/userdb.txt cowrievolumes/$CONTAINER_NAME/data/
-	cp "$(pwd)"cowrie/cowrie.cfg.dist cowrievolumes/$CONTAINER_NAME/cowrie.cfg
+	docker build -t amhiggin/cowrie .
+        # Create the required host directories if they don't already exist
+        mkdir -p cowrievolumes/$CONTAINER_NAME/dl       # Capture malware checksums
+        mkdir -p cowrievolumes/$CONTAINER_NAME/log      # Capture logs
+        mkdir -p cowrievolumes/$CONTAINER_NAME/data     # Add custom userdb.txt
+
+        # Copy the userdb.txt and cowrie.cfg files into the volume being mounted
+        wget -nc https://raw.githubusercontent.com/micheloosterhof/cowrie/master/cowrie.cfg.dist
+        wget -nc https://raw.githubusercontent.com/micheloosterhof/cowrie/master/data/userdb.txt
+        cp "$(pwd)"/userdb.txt cowrievolumes/$CONTAINER_NAME/data/
+        cp "$(pwd)"/cowrie.cfg.dist cowrievolumes/$CONTAINER_NAME/cowrie.cfg
+
 }
 
 # Run the docker container in the background, giving it name "CONTAINER_NAME"
@@ -37,16 +60,21 @@ stop() {
 
 # Remove the docker container
 remove() {
-	echo "Removing container $CONTAINER_NAME" && \
-		docker rm -f $CONTAINER_NAME &> /dev/null || true
+        echo "Removing container $CONTAINER_NAME" && \
+                docker rm -f $CONTAINER_NAME &> /dev/null || true
+        echo "Preserving container directories - delete manually if required"
+
 }
 
 create_dmz_net() {
-	network_exists=$( sudo docker network ls | grep "dmz" )	
-	if [[ -n "$result" ]] ; then
-		echo "DMZ network defined"
-	else
-		echo "Creating DMZ network (10.0.0.0/8)"
-		docker network create -d bridge --subnet 10.0.0.0/8 dmz
-	fi
+        network_exists=$( sudo docker network ls | grep "dmz" ) 
+        if [[ -n "$network_exists" ]] ; then
+                echo "DMZ network defined"
+        else
+                echo "Creating DMZ network (10.0.0.0/8)"
+                docker network create -d bridge --subnet 10.0.0.0/8 dmz
+        fi
 }
+
+main
+
