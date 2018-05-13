292a3ac68723755169c7ff80d245875d4d4a9d95
Amber <amhiggin@tcd.ie>
2018-05-13 20:25:55 +0100

    Remove comments in cowrie entrypoint script

--- cowrie/start.sh
+++ cowrie/start.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Update default route on eth0
-#echo "Reconfiguring routing table.." && echo "$(route)"
-#route del default eth0
-#route add -net 10.0.0.0 netmask 255.0.0.0 dev eth0
-#route add default gw 10.0.0.254 netmask 255.0.0.0 dev eth0
-#echo "Routing table now looks like " && echo "$(route)"
-
 # Allow cowrie to listen as non-root on ports 22 and 23
 touch /etc/authbind/byport/22 && \
     touch /etc/authbind/byport/23 && \
